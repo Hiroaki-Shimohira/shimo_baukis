393644cfe9376f779c946df18658c51d0606f21f
first <hiroakishimohira@hiroakinoMacBook-Pro.local>
2017-02-14 23:41:14 +0900

    9-4

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,41 @@
+class Admin::SessionsController < Admin::Base
+  def new
+    if current_administrator
+      redirect_to admin_root_path
+    else
+      @form = Admin::LoginForm.new
+    end
+  end
+
+  def create
+    @form = Admin::LoginForm.new(form_params)
+    if @form.email.present?
+      admin = Administrator.find_by(email_for_index: @form.email.downcase)
+    end
+
+    if Admin::Authenticator.new(admin).authenticate
+      if admin.suspended?
+        flash.alert = "アカウントが停止されています"
+        redirect_to admin_login_path
+      else
+        session[:administrator_id] = admin.id
+        flash.notice ="ログインしました"
+        redirect_to admin_root_path
+      end
+    else
+      flash.now.alert = "メアドが正しくありません"
+      render "new"
+    end
+  end
+
+  def destroy
+    session.delete(:administrator_id)
+    flash.notice = "ログアウトしました"
+    redirect_to admin_root_path
+  end
+
+  private
+    def form_params
+      params.require(:admin_login_form).permit(:email)
+    end
+end
--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -6,22 +6,34 @@ class Staff::SessionsController < Staff::Base
       @form = Staff::LoginForm.new
     end
   end
+
   def create
     @form = Staff::LoginForm.new(form_params)
     if @form.email.present?
       staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
     end
+
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-      session[:staff_member_id]=staff_member.id
-      redirect_to staff_root_path
+      if staff_member.suspended?
+        flash.alert = "アカウントが停止されています"
+        redirect_to staff_login_path
+      else
+        session[:staff_member_id]=staff_member.id
+        flash.notice = "ログインしました"
+        redirect_to staff_root_path
+      end
     else
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません"
       render "new"
     end
   end
+
   def destroy
     session.delete(:staff_member_id)
+    flash.notice = "ログアウトしました"
     redirect_to staff_root_path
   end
+
   private
   def form_params
     params.require(:staff_login_form).permit(:email,:password)
